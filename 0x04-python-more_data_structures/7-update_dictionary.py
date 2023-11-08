ec47e427ccdd41352ba15282b8c8c6f16466d221
Ayoub <sb.naba2@gmail.com>
2023-11-08 11:19:49 -0800

    a function that replaces or adds key/value in a dictionary.

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def update_dictionary(a_dictionary, key, value):
+    for keys in a_dictionary.keys():
+        if keys == key:
+            a_dictionary[key] = value
+            continue
+    if key not in a_dictionary.keys():
+        a_dictionary[key] = value
+    return a_dictionary
+
