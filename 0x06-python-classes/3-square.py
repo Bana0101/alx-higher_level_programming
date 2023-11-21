f6dcc5d4d7fa41f35306c7f801b76554c6b33a70
Ayoub <sb.naba2@gmail.com>
2023-11-21 11:48:24 -0800

    returns the current square area

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -17,6 +17,7 @@ class Square:
         elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
+
     # returns the current square area
     def area(self):
         return self.__size ** 2
