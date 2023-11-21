ffdc39c9c9eee133dd89ecbc6155a9a66410e0e5
Ayoub <sb.naba2@gmail.com>
2023-11-21 12:49:05 -0800

    using setter and getter property

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -28,7 +28,6 @@ class Square:
             raise ValueError("size must be >= 0")
         self.__size = value
 
-    @property
     # returns the current square area
     def area(self):
         return self.__size ** 2
