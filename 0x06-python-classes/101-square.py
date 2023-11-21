01d29fe511fc5ab6d68bc9742572e20855914811
Ayoub <sb.naba2@gmail.com>
2023-11-21 15:48:54 -0800

    Printing a Square instance with the same behavior as my_print()

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -62,6 +62,8 @@ class Square:
         if self.__size != 0:
             for _ in range(self.__position[1]):
                 print()
-            for _ in range(self.__size):
-                print(" " * self.__position[0] + "#" * self.__size)
+        for x in range(self.__size):
+            print(" " * self.__position[0] + "#" * self.__size, end="")
+            if x != self.__size - 1:
+                print()
         return ("")
