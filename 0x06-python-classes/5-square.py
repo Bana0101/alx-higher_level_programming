4c7567e708d1a13e3fdb380cb0462ba5652b4f18
Ayoub <sb.naba2@gmail.com>
2023-11-21 13:04:52 -0800

    public instance methode that can print a square

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""Square."""
+
+
+class Square:
+    """Defines a square."""
+    def __init__(self, size=0):
+        """
+        initialize a new instance
+        Attribute:
+        - size (int): the size of the square
+        """
+        self.size = size
+
+    # Getter methode for the size
+    @property
+    def size(self):
+        return self.__size
+
+    # Setter methode for the size
+    @size.setter
+    def size(self, value):
+        # size must be an integer
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        # size must be positive
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    # returns the current square area
+    def area(self):
+        return self.__size ** 2
+
+    # Print the square with the character '#'
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        else:
+            for x in range(self.__size):
+                print("#" * self.__size)
