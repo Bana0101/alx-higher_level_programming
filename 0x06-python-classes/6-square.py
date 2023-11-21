a14e3eb97f9172365e5ea9d114e80c3d160010b5
Ayoub <sb.naba2@gmail.com>
2023-11-21 15:00:19 -0800

    position should be use by using space

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -53,7 +53,7 @@ class Square:
         if self.__size == 0:
             print()
         else:
-            if self.__position[1] > 0:
+            for _ in range(self.__position[1]):
                 print()
             for x in range(self.__size):
                 print(" " * self.__position[0] + "#" * self.__size)
