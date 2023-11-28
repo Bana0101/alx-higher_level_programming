544bcb973f6378fd3465c2271845cfa923ea7428
Ayoub <sb.naba2@gmail.com>
2023-11-27 17:05:21 -0800

    Define a class attribute used as symbol for string representation

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -58,7 +58,7 @@ class Rectangle:
     def __str__(self):
         if self.width == 0 or self.height == 0:
             return ""
-        return ((str(self.print_symbol) * self.width + "\n") * self.height)[:]
+        return ((str(self.print_symbol) * self.width + "\n") * self.height)[:-1]
 
     def __repr__(self):
         return f"Rectangle({self.width}, {self.height})"
