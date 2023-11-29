f3c8d5b34b1a4d99ff3ed6ec7bba3cbc819adcb9
Ayoub <sb.naba2@gmail.com>
2023-11-29 14:59:57 -0800

    a function that add 2 integer

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python3
-""" Module documentation """
+"""
+Module documentation
+
+this module provide a function for adding two integers
+"""
+
 import doctest
 
 
 def add_integer(a, b=98):
-    """ A fucntion that add two integer """
+    """
+    A fucntion that add two integer
+    """
     if not isinstance(a, float) and not isinstance(a, int):
         raise TypeError("a must be an integer")
     if not isinstance(b, float) and not isinstance(b, int):
