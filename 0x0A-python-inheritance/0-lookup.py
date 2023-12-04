f66415a1076f6b84c4cc82145830c6bda6ea97bd
Ayoub <sb.naba2@gmail.com>
2023-12-04 09:28:14 -0800

    a function that returns the list of available attributes and methods

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -5,3 +5,8 @@
 def lookup(obj):
     """a function that returns the list of available attributes and methods"""
     return list(dir(lookup))
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/0-lookup.txt")
