2022dfaf3d01989a76201a46c49671a7e055182e
Ayoub <sb.naba2@gmail.com>
2023-12-05 10:57:19 -0800

    a function that read a text file and print it

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" module documentation """
+
+
+def read_file(filename=""):
+    """ function that reads a text file """
+    with open(filename, 'r') as fd:
+        print(fd.read())
+
+    
