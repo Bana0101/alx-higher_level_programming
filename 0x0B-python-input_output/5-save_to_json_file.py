e81c930a940c632a7df100a02ba62e47ceaf1c66
Ayoub <sb.naba2@gmail.com>
2023-12-05 12:55:57 -0800

    a function that writes an object to a text file

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -7,9 +7,3 @@ def save_to_json_file(my_obj, filename):
     """ function that reads a text file """
     with open(filename, 'w') as fd:
         json.dump(my_obj, fd)
-
-def set_default(obj):
-    """ convert a set to a list for json serialization"""
-    if isinstance(obj, set):
-        return list(obj)
-    raise TypeError("the object is not JSON serialization")
