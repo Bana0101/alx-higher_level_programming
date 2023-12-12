377d52f246c22fd77cd2bfdf63eabda2cf36077f
Ayoub <sb.naba2@gmail.com>
2023-12-12 06:35:22 -0800

    the class Rectangle that inherits from Base

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -5,7 +5,7 @@ from models.base import Base
 
 class Rectangle(Base):
     """ new class that inherits from Base class """
-    def __init__(self, width, height, x=0, y=0, id=None): 
+    def __init__(self, width, height, x=0, y=0, id=None):
         super().__init__(id)
         self.width = width
         self.height = height
@@ -20,10 +20,10 @@ class Rectangle(Base):
     def width(self, value):
         self.__width = value
 
-    @property                      
+    @property
     def height(self):
         return self.__height
- 
+
     @height.setter
     def height(self, value):
         self.__height = value
