262923e6a55ea8b05d9344e5418c76b9ed3b7156
ayoub <sb.naba2@gmail.com>
2024-03-05 18:19:25 +0100

    network_0

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # sends a GET request to the URL, and displays the body of the response 
-curl -I -s -X "${1}" | cut -d " " -f 2-
+curl -I -s "${1}" |grep "^Allow: .*" | cut -d " " -f 2-
